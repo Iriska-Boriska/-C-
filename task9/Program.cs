51ee9839a7e068ded0464c8323aa1f4007f904a4
boriska.batakov <boriska.batakov@mail.ru>
2022-08-03 16:31:26 +0300

    Solved task9

---
+++ task9/Program.cs
@@ -0,0 +1,6 @@
+﻿//9. Вывести на экран четные числа от 1 до N
+Console.Clear();
+System.Console.Write("Введи целое число больше 1: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1 ;i < N+1; i++)
+System.Console.Write($"{i}, ");
