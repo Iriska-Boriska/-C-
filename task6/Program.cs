7294d2d0ebde523cdaa10cc332ce1c84be1156f8
boriska.batakov <boriska.batakov@mail.ru>
2022-08-03 15:57:06 +0300

    Solved task6

---
+++ task6/Program.cs
@@ -0,0 +1,6 @@
+﻿//Написать программу вычисления значения функции y = sin(a). (Класс Math). 
+Console.Clear();
+System.Console.Write ("Введи угол в градусах: ");
+double AngleInDegrees = Convert.ToDouble (Console.ReadLine());
+double AngleInRadias = AngleInDegrees/180*Math.PI;
+System.Console.WriteLine (Math.Round (Math.Sin(AngleInRadias),2));
