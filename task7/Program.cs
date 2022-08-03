de54aeab3e4a9a5ff5a9d908934100ebfc69a8d2
boriska.batakov <boriska.batakov@mail.ru>
2022-08-03 16:10:35 +0300

    Solved task7

---
+++ task7/Program.cs
@@ -0,0 +1,9 @@
+﻿//7. Выяснить является ли число чётным.
+Console.Clear();
+System.Console.WriteLine("Я проверю чётное ли число./nВведи число:");
+System.Console.WriteLine("Введи целое число: ");
+int Number = Convert.ToInt32 (Console.ReadLine());
+if (Number%2 == 0)
+    System.Console.WriteLine("Число чётное");
+else
+    System.Console.WriteLine("Число не чётное");
