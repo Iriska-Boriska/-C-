1414ceb4076ab07405d9c58e9e6a04976b605dad
boriska.batakov <boriska.batakov@mail.ru>
2022-08-03 16:27:29 +0300

    Solved task8

---
+++ task8/Program.cs
@@ -0,0 +1,7 @@
+﻿//8. Вывести на экран числа от -N до N
+Console.Clear();
+System.Console.Write("Введи целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = N*(-1);
+for (;i < N+1; i++)
+System.Console.Write($"{i}, ");
