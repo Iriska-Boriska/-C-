ba91217055e1d50bc9c263d612886300114f1b3b
boriska.batakov <boriska.batakov@mail.ru>
2022-08-03 11:01:51 +0300

    Solved task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+
+Console.Clear();
+double a = 0;
+double b = 0;
+double c = 0;
+double Max = 0;
+System.Console.WriteLine ("Введи 3 числа: ");
+System.Console.Write ("a = ");
+a =Convert.ToInt32( Console.ReadLine ());
+System.Console.Write ("b = ");
+b =Convert.ToInt32( Console.ReadLine ());
+System.Console.Write ("c = ");
+c =Convert.ToInt32( Console.ReadLine ());
+Max = a;
+if (b > Max) Max = b;
+if (c > Max) Max = c;
+System.Console.WriteLine($"Большее из них: {Max}");
