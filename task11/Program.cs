e1e9b69518d1f1ecfff0e6a4e7c4622e9a793393
boriska.batakov <boriska.batakov@mail.ru>
2022-08-04 15:14:45 +0300

    Solved task11

---
+++ task11/Program.cs
@@ -0,0 +1,6 @@
+﻿// 11. Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+
+Console.Clear();
+System.Console.WriteLine("Введи число больше 9: ");
+int X = Convert.ToInt32 (Console.ReadLine ());
+System.Console.WriteLine(X/10%10);
