6f76b724efe02a8927efdaedf0d821927b161672
boriska.batakov <boriska.batakov@mail.ru>
2022-08-04 15:06:29 +0300

    Solved task10

---
+++ task10/Program.cs
@@ -0,0 +1,5 @@
+﻿//10. Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+Console.Clear();
+System.Console.Write ("Введи число: ");
+int X = Convert.ToInt32 (Console.ReadLine());
+System.Console.WriteLine(X%10);
