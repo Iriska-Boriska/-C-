09d5f62465dee733178a8725f753f789ea983ece
boriska.batakov <boriska.batakov@mail.ru>
2022-08-05 14:45:19 +0300

    Solved task15

---
+++ task15/Program.cs
@@ -0,0 +1,10 @@
+﻿// 15. С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет
+Console.Clear ();
+System.Console.Write ("Введи целое число: ");
+string Namber = Console.ReadLine();
+if (Convert.ToInt32(Namber) > 99)
+    System.Console.WriteLine(Namber.Substring(2, 1));
+else
+    System.Console.WriteLine("В указанном числе нет 3 цифры");
+
+
