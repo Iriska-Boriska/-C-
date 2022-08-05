48df9ae29fe480092da9cbba892cb5eba500b89e
boriska.batakov <boriska.batakov@mail.ru>
2022-08-05 12:25:04 +0300

    Solved task13

---
+++ task13/Program.cs
@@ -0,0 +1,5 @@
+﻿// 13. Удалить вторую цифру целого числа введенного с клавиатуры.
+Console.Clear();
+System.Console.Write ("Введи целое число больше 9: ");
+string Number = (Console.ReadLine());
+System.Console.WriteLine( Number.Remove (1, 1));
