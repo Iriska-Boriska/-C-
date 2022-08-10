eaca317d8e41eed49124166c7516964451bc2a0e
boriska.batakov <boriska.batakov@mail.ru>
2022-08-10 12:17:50 +0300

    Solved task16

---
+++ task16/Program.cs
@@ -0,0 +1,10 @@
+﻿// 16. Дано число. Проверить кратно ли оно 7 и 23
+Console.Clear();
+System.Console.WriteLine("Введи число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+bool N7 = Number%7 == 0;
+bool N23 = Number%23 == 0;
+if (N7 && N23)
+    System.Console.WriteLine($"{Number} кратно одновременно 7 и 23");
+else
+    System.Console.WriteLine($"{Number} НЕ кратно одновременно 7 и 23");
