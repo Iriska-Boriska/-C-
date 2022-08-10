d4013558ea7278fab56736220c975ebcee42f83f
boriska.batakov <boriska.batakov@mail.ru>
2022-08-10 18:58:33 +0300

    Fixed task22

--- task22/Program.cs
+++ task22/Program.cs
@@ -1,14 +1,10 @@
 ﻿// 22. Программа проверяет пятизначное число на палиндромом.
 Console.Clear();
-/*
-int ConvtrtToPalindrome5 (int A)
-{
-    int End = (A%100000) + (A%10000) + (A%1000) + (A%100) + (A%10);
-    return End;
-}
-*/
-
 System.Console.Write("Введи пятизначное число: ");
 int Number = Convert.ToInt32(Console.ReadLine());
-int Palindrome = (Number%10)*10000 + (Number%100/10)*1000 + (Number%1000/100)*100 + (Number%10000/1000)*10 + (Number%100000/10000);
-System.Console.WriteLine($"      Вот его палиндром: {Palindrome}");
+int InvertedNumber = (Number%10)*10000 + (Number%100/10)*1000 + (Number%1000/100)*100 + (Number%10000/1000)*10 + (Number%100000/10000);
+if (Number == InvertedNumber)
+    System.Console.WriteLine($"{Number} - палиндром");
+else
+    System.Console.WriteLine($"{Number} - не палиндром");
+// Можно было проще, но я сначала подумал, что палиндром это просто когда переворачивают число, а не зеркалное. 
