f1c77a95a85b08f0c3a7725203f026e20c5f01f2
boriska.batakov <boriska.batakov@mail.ru>
2022-08-10 17:04:35 +0300

    Solved task19

---
+++ task19/Program.cs
@@ -0,0 +1,33 @@
+﻿// 19. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y 
+//                                      !(X || Y)==!X && !Y
+
+Console.Clear();
+bool X = true;
+bool Y = true;
+System.Console.WriteLine($"   X  |   Y   | Результат !(X || Y)==!X && !Y ");
+System.Console.WriteLine($" {X} | {Y} |             {!(X || Y)==!X && !Y}");
+X=!X;
+System.Console.WriteLine($" {X} | {Y} |             {!(X || Y)==!X && !Y}");
+X=!X;
+Y=!Y;
+System.Console.WriteLine($" {X} | {Y} |             {!(X || Y)==!X && !Y}");
+X=!X;
+System.Console.WriteLine($" {X} | {Y} |             {!(X || Y)==!X && !Y}");
+
+/*
+Console.Clear();
+bool X = true;
+bool Y = true;
+int i = 0;
+System.Console.WriteLine($"  X  |  Y  | Результат !(X || Y)==!X && !Y ");
+while (i==3)
+    System.Console.WriteLine($" {X} | {Y} |             {!(X || Y)==!X && !Y}");
+    if (i==1)
+        X=!X;
+    if (i==2)
+        X=!X;
+        Y=!Y;
+    if (i==3)
+        X=!X;
+    i++;
+*/
