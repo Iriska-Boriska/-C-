a131acb242d4ed56d3b389c5bc7d6fa408c1c703
boriska.batakov <boriska.batakov@mail.ru>
2022-08-10 18:04:44 +0300

    Solved task21

---
+++ task21/Program.cs
@@ -0,0 +1,12 @@
+﻿// 21. Задать номер четверти, показать диапазоны для возможных координат
+Console.Clear();
+System.Console.Write("Введи номер четверти: ");
+int QuarterNumber = Convert.ToInt32(Console.ReadLine());
+if (QuarterNumber == 1)
+    System.Console.WriteLine($"В {QuarterNumber} четверть входят координаты больше X0 и больше Y0");
+if (QuarterNumber == 2)
+    System.Console.WriteLine($"В {QuarterNumber} четверть входят координаты меньше X0 и больше Y0");
+if (QuarterNumber == 3)
+    System.Console.WriteLine($"В {QuarterNumber} четверть входят координаты меньше X0 и меньше Y0");
+if (QuarterNumber == 4)
+    System.Console.WriteLine($"В {QuarterNumber} четверть входят координаты больше X0 и меньше Y0");
