ee91eae64d46e94715662415721731a744d2cdc8
boriska.batakov <boriska.batakov@mail.ru>
2022-08-12 12:14:50 +0300

    Solved task26

---
+++ task26/Program.cs
@@ -0,0 +1,9 @@
+﻿// 26. Найти сумму чисел от 1 до А
+Console.Clear();
+System.Console.WriteLine("Я покажу сумму чисел от 1 до А включительно.");
+System.Console.Write("Введи A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int Sam = 0;
+for (int i=0; i<=A; i++)
+    Sam = Sam + i;
+System.Console.WriteLine(Sam);
