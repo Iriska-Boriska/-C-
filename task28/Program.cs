3be189d67bdb249f05c8bf5b7a59f43f8b8dfae0
boriska.batakov <boriska.batakov@mail.ru>
2022-08-12 13:00:01 +0300

    Solved task28

---
+++ task28/Program.cs
@@ -0,0 +1,12 @@
+﻿// 28. Определить количество цифр в числе. Сделать подпрограмму.
+int LengthNanber (int Nanber)
+{
+    int Amount = Convert.ToString(Nanber).Length;
+    return Amount;
+}
+Console.Clear();
+//Length
+System.Console.WriteLine("Я покажу кол-во цифр в числе N.");
+System.Console.Write("Введи N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(LengthNanber (N));
