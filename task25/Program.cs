2f229c161d130e856a6b034e58fe77595613fed3
boriska.batakov <boriska.batakov@mail.ru>
2022-08-12 12:05:49 +0300

    Solved task25

---
+++ task25/Program.cs
@@ -0,0 +1,6 @@
+﻿// 25. Вывести на экран кубы чисел от 1 до N
+Console.Clear();
+System.Console.Write("Введи N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i=1; i<=N; i++)
+    System.Console.WriteLine($"Куб {i} = {i*i*i}");
