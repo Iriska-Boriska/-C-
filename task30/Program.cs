068388483a88f68557afc0358f08fe0e877fdbb2
boriska.batakov <boriska.batakov@mail.ru>
2022-08-15 15:16:09 +0300

    Solved task30

---
+++ task30/Program.cs
@@ -0,0 +1,9 @@
+﻿// 30. Написать программу вычисления произведения чисел от 1 до N
+Console.Clear();
+System.Console.WriteLine("Я покажу произведение всех чисел от 1 до введённого числа.");
+System.Console.WriteLine("Введи целое число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for(int i=2; i<=Number; i++)
+    result = result*i;
+System.Console.WriteLine(result);
