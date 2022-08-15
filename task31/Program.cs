bd1b8b48341d92b7439fc628272237123381ecf9
boriska.batakov <boriska.batakov@mail.ru>
2022-08-15 17:35:35 +0300

    Solved task31

---
+++ task31/Program.cs
@@ -0,0 +1,16 @@
+﻿// 31. Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+System.Console.WriteLine("Кубы чисел, заканчивающихся на четную цифру:");
+int LastValue = 100;
+int Cube = 0;
+int i = 0;
+while (i<=LastValue)
+{
+    Cube = i*i*i;
+    if (Cube%2 == 0)
+    {
+        System.Console.WriteLine($"{i}^3 = {Cube}");
+    }
+    Cube = 0;
+    i++;
+}
