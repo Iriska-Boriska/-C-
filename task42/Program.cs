a0d916d28082f4f19755e4267d40a8e97a41cf98
boriska.batakov <boriska.batakov@mail.ru>
2022-08-19 13:15:55 +0300

    Solved task42

---
+++ task42/Program.cs
@@ -0,0 +1,24 @@
+﻿// 42. Выяснить являются ли три числа сторонами треугольника
+Console.Clear();
+System.Console.WriteLine("Введи ниже через пробел длины сторон треугольниуов:");
+int[] Num = new int[3];
+EnteringIntsOnOneLineArr(Num, ' ');
+
+if (Num[0] + Num[1] > Num[2]
+ && Num[2] + Num[0] > Num[1]
+ && Num[1] + Num[2] > Num[0])
+{
+    System.Console.WriteLine("Числа являются сторонами треугольника");
+}
+else { System.Console.WriteLine("Числа НЕ являются сторонами треугольника"); }
+
+void EnteringIntsOnOneLineArr(int[] SNumbers, char separator) // Наполняет массив введёнными в консоль числами разделёнными separator
+{
+    string S = Console.ReadLine();
+    string[] SText = S.Split(separator);
+    for (int i = 0; i < SNumbers.Length; i++)
+    {
+        SNumbers[i] = int.Parse(SText[i]);
+    }
+}
+
